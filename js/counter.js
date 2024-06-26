0c35b74286a6dbb7f0a213d079a25ae87a2bc6d4
abila98 <abilanadar1998@gmail.com>
2024-06-26 15:26:31 +0000

    Added views

--- js/counter.js
+++ js/counter.js
@@ -4,8 +4,7 @@ async function updateCounter() {
     console.log("Updating counter...");
     let response = await fetch("https://jul7qeeflj4ecbjptkrxfefon40qfiob.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `<p>This page has ${data} Views!</p>`;
+    counter.innerHTML = `<h5>This page has ${data} Views!</h5>`;
 }
 
 updateCounter();
-
